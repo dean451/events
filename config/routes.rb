68fde010e47716a0f8fa6203c1b6ab1fa41cdd43
Dean Palm <dean451@gmail.com>
2016-10-02 22:23:37 -0500

    omg the features are working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users 
+  devise_for :users
   root to: 'events#index'
   get "events" => "events#index"
-  get "events/:id" =>"events#show"
+  get "events/:id" =>"events#show", as: "event"
+  get "events/:id/edit" =>"events#edit", as: "edit_event"
+  patch "events/:id" =>"events#update"
 end
