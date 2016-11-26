ecc6c862133f51e2ac1c2bbbc188eadf18fb3249
Dean Palm <dean451@gmail.com>
2016-11-25 21:03:24 -0600

    registrations controller and form errors, nested routes too

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :registrations
+
   devise_for :users
   root to: 'events#index'
-  # get "events" => "events#index"
-  # get "events/:id" =>"events#show", as: "event"
-  # get "events/:id/edit" =>"events#edit", as: "edit_event"
-  # patch "events/:id" =>"events#update"
-  resources :events
+
+  resources :events do
+    resources :registrations
+  end
 end
